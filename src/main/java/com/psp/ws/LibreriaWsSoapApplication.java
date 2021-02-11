44cacfe83370d163d6cc7492499def86e4a4cb88
paulacarm <cm.paula92@gmail.com>
2021-02-11 17:51:37 +0100

    1

---
+++ src/main/java/com/psp/ws/LibreriaWsSoapApplication.java
@@ -0,0 +1,13 @@
+package com.psp.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaWsSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaWsSoapApplication.class, args);
+	}
+
+}
